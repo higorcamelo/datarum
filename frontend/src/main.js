70daeff3f0c0b740e20d186d73278add5b08be19
Higor <higorcamelo@gmail.com>
2026-03-08 20:00:17 -0300

    Ajustes para deploy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import LandingPage from './views/LandingPage.vue'
-import AppConverter from './views/ConversorApp.vue'
+import router from './router'
 import './style.css'
-const routes = [
-  { path: '/', component: LandingPage },
-  { path: '/conversor', component: AppConverter }
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
 
 createApp(App).use(router).mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LandingPage from '../views/LandingPage.vue'
 import ConversorApp from '../views/ConversorApp.vue'
 
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
