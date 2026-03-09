c59debb26588d8480507bffe7670f001d3e5212f
Higor Camelo <higorcamelo@outlook.com>
2026-03-09 09:01:15 -0300

    Atualização do vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/datarum/',
+  base: '/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
