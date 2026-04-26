103811d0a393df0955eab83ed2562c8ddc3583ae
Higor <higorcamelo@gmail.com>
2026-04-26 00:50:39 -0300

    Update ConversorApp.vue

--- frontend/src/views/ConversorApp.vue
+++ frontend/src/views/ConversorApp.vue
@@ -308,7 +308,7 @@ const enviarArquivos = async () => {
   const timeoutId = setTimeout(() => controller.abort(), 60000);
 
   try {
-    const response = await fetch(`${GCP_KEY}${ENDPOINTS.converter}`, {
+    const response = await fetch(`${GCP_KEY}${ENDPOINTS.processar}`, {
       method: 'POST',
       body: formData,
       signal: controller.signal
